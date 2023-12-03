6bbf7e9883501a22c37091dfc47930284ae0d796
JonBay <134355923+JonBay@users.noreply.github.com>
2023-12-03 15:16:04 -0700

    fixed product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,7 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 //get all products
 router.get("/", async (req, res) => {
   try {
-    const productData = await Category.findAll({
+    const productData = await Product.findAll({
       include: [{ model: Category }],
     });
     res.status(200).json(productData);
